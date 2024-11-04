1fd36ba764ae8d12fc22e2e89cfc3e73cfb28650
ct-chou <63345621+ct-chou@users.noreply.github.com>
2024-11-04 01:41:10 -0500

    Feat: created Class TodoItem

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import { greeting } from "./todo";
+import { greeting, TodoItem } from "./todo";
 import "./style.css";
-
+// import { storageAvailable } from "./local_storage";
 import { compareAsc, format } from "date-fns";
+
+console.log(greeting);
+
+// const item1 = newItem("Buy groceries", "Milk, Cheese, Pizza, Fruit, Tylenol", "2021-12-31", "High");
+const item1 = new TodoItem("Buy groceries", "Milk, Cheese, Pizza, Fruit, Tylenol", "2021-12-31", "High");
+
+console.log(item1.title);
+item1.priority = "Low";
+console.log(item1.priority);
 
 format(new Date(2014, 1, 11), "yyyy-MM-dd");
 //=> '2014-02-11'
@@ -19,4 +28,3 @@ console.log(dates);
 //   Sun Jul 02 1995 00:00:00
 // ]
 
-console.log(greeting);
