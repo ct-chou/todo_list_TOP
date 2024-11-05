d707f20e56eaccfe6c94daf7cdfa2a6fc3342de2
ct-chou <63345621+ct-chou@users.noreply.github.com>
2024-11-04 23:27:49 -0500

    Feat: add delete todo item from project function

--- src/projects.js
+++ src/projects.js
@@ -4,6 +4,15 @@ export function add_to_project(project, item) {
     project.items.push(item);
 }
 
+export function delete_from_project(project, item) {
+    const index = project.items.indexOf(item);
+    project.items.splice(index, 1);
+    if (project.items.length === 0) {
+        const index = all_projects.indexOf(project);
+        all_projects.splice(index, 1);
+    }
+}
+
 function check_existing_project(name) {
     for (let i = 0; i < all_projects.length; i++) {
         if (all_projects[i].title === name) {
